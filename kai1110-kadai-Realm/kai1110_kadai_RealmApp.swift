d5e7febe2cec3623a1ab46c5683ec06bbbf2e7f4
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-07 14:35:03 +0900

    Initial Commit

---
+++ kai1110-kadai-Realm/kai1110_kadai_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  kai1110_kadai_RealmApp.swift
+//  kai1110-kadai-Realm
+//
+//  Created by 渡邊魁優 on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct kai1110_kadai_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
